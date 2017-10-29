0d935df17ebe398ff35861666dafbc175bb16fe3
Raghavendra Venkatesh <rvenkatesh@sessionm.com>
2017-10-28 21:23:12 -0400

    colorChange code

--- public/client/views/colors/controllers/colors.controller.client.js
+++ public/client/views/colors/controllers/colors.controller.client.js
@@ -7,6 +7,27 @@
         var vm = this;
 
         function init() {
+            var img = new Image();
+            img.src = '../../image/deep_warm.png';
+            var canvas = document.getElementById('warm-canvas');
+            var ctx = canvas.getContext('2d');
+            img.onload = function() {
+                ctx.drawImage(img, 0, 0);
+                img.style.display = 'none';
+            };
+            var color = document.getElementById('color');
+            function pick(event) {
+                var x = event.layerX;
+                var y = event.layerY;
+                var pixel = ctx.getImageData(x, y, 1, 1);
+                var data = pixel.data;
+                var rgba = 'rgba(' + data[0] + ', ' + data[1] +
+                    ', ' + data[2] + ', ' + (data[3] / 255) + ')';
+                color.style.background =  rgba;
+                color.textContent = rgba;
+                console.log(rgba);
+            }
+            canvas.addEventListener('mousemove', pick);
         }
         init();
 
