f5aedf283bed221961c7fe80af0316b8b8cb40c1
Raghavendra Venkatesh <rvenkatesh@sessionm.com>
2017-10-28 20:14:43 -0400

    Port change

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use(express.static(__dirname + '/public'));
 
 require("./server/app.js")(app);
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port);
